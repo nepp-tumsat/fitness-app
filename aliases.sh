b867044cf82aa979017da9b5595ca5acbab47ed4
sasaki404 <takaaki.sasaki2001@gmail.com>
2021-11-01 15:39:41 +0900

    [add] aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias fitness-api='docker-compose up api'
+alias fitness-front='docker-compose up front'
+alias db-create='docker-compose run api rails db:create'
+alias db-migrate='docker-compose run api rails db:migrate'
