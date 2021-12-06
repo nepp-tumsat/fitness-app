4cdf4d66b72203b41f44872f7069ec5233ffaf84
sasaki404 <takaaki.sasaki2001@gmail.com>
2021-12-07 08:50:27 +0900

    [fix] whereで指定しているカラムの重複

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,33 +1,28 @@
 # coding: utf-8
 User.where(:email => "hogehoge@gmail.com").first_or_create(
     :name => "hogehoge",
-    :email => "hogehoge@gmail.com",
     :password => "hogehoge1234",
     :tall => 175.4 ,
     :birthday => "2000-01-01")
 
 User.where(:email => "hogehoge2@gmail.com").first_or_create(
     :name => "hogehoge2",
-    :email => "hogehoge2@gmail.com",
     :password => "hogehoge1234",
     :tall => 175.4 ,
     :birthday => "2000-01-01")
 
 
 UserBodyComposition.where(:user_id => 1).first_or_create(
-    :user_id => 1,
     :weight => 67.2,
     :body_fat_rate => 23.4
 )
 
 UserBodyComposition.where(:user_id => 2).first_or_create(
-    :user_id => 2,
     :weight => 64.8,
     :body_fat_rate => 16.7
 )
 
 Target.where(:user_id => 1).first_or_create(
-    :user_id => 1,
     :weight => 59.5,
     :body_fat_rate => 15.0,
     :daily_energy => 2000,
@@ -36,7 +31,6 @@ Target.where(:user_id => 1).first_or_create(
 )
 
 Target.where(:user_id => 2).first_or_create(
-    :user_id => 2,
     :weight => 60.0,
     :body_fat_rate => 15.0,
     :daily_energy => 2000,
