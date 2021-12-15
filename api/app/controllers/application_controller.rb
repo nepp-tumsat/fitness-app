32967dde3707b57c686c68b487b6a81fc7a2ea03
sasaki404 <takaaki.sasaki2001@gmail.com>
2021-12-15 12:37:17 +0900

    [fix] targets_controller

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :authenticate_account!, unless: :devise_controller?
+  before_action :authenticate_user!, except: [:new, :create]
   before_action :configure_permitted_parameters, if: :devise_controller? 
 
   respond_to :json
