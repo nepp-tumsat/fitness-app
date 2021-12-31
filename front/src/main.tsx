63232b991ffce2d29f4c915e311eb080b264eb79
sasaki404 <takaaki.sasaki2001@gmail.com>
2021-12-31 11:48:25 +0900

    [add] frontと接続してログイン

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,21 +1,10 @@
 import { render } from 'react-dom'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom'
 import './index.css'
-import Home from './views/Home'
-import Header from './components/Header'
+import App from './App'
 
 const rootElement = document.getElementById('root')
 
 render(
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
+  <App/>,
   rootElement
 )
--- front/src/views/Home.tsx
+++ front/src/views/Home.tsx
@@ -3,10 +3,41 @@ import WeightCard from '../components/WeightCard'
 import BfpCard from '../components/BfpCard'
 import PfcBalanceCard from '../components/PfcBalanceCard'
 import FitnessCard from '../components/FitnessCard'
+import React, { useContext } from 'react';
+import { AuthContext } from '../App';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      userInfos: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
 
 export default function Home() {
+  const { isSignedIn, currentUser } = useContext(AuthContext)
+  const UserInfos = () => {
+    if (isSignedIn && currentUser) {
+      return (
+        <div>
+            <h2>Email: {currentUser?.email}</h2>
+            <h2>Name: {currentUser?.name}</h2>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          {isSignedIn}
+          {currentUser}
+        </div>
+      )
+    }
+  }
   return (
     <Box mt={2} mx={50} >
+    <UserInfos />
     <Grid container spacing={2}>
       <Grid item xs={6}>
         <WeightCard />
